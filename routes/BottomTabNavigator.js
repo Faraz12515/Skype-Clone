845bafbec981eebf770448274e2080fceb118333
SYED FARAZ ALI <alifaraz7866@gmail.com>
2021-03-27 21:15:31 +0500

    BackHandler added to app

--- routes/BottomTabNavigator.js
+++ routes/BottomTabNavigator.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {BackHandler, Alert} from 'react-native';
+import {useIsFocused} from '@react-navigation/native';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Chats, Calls, Contacts} from '../screens';
 
 export default function BottomTabNavigator() {
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    console.log(isFocused);
+    if (isFocused) {
+      BackHandler.addEventListener('hardwareBackPress', backAction);
+    }
+
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+  }, [isFocused]);
+
+  const backAction = () => {
+    Alert.alert('Hold on!', 'Are you sure you want to Exit?', [
+      {
+        text: 'Cancel',
+        onPress: () => null,
+        style: 'cancel',
+      },
+      {text: 'YES', onPress: () => BackHandler.exitApp()},
+    ]);
+    return true;
+  };
   const Tab = createBottomTabNavigator();
 
   return (
