f9cbc429c965ff6441fa06e38413ee042bcbc4ea
SYED FARAZ ALI <alifaraz7866@gmail.com>
2021-03-28 21:59:07 +0500

    Working on Chats Tab

--- routes/BottomTabNavigator.js
+++ routes/BottomTabNavigator.js
@@ -4,6 +4,8 @@ import {useIsFocused} from '@react-navigation/native';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Chats, Calls, Contacts} from '../screens';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function BottomTabNavigator() {
   const isFocused = useIsFocused();
@@ -32,10 +34,64 @@ export default function BottomTabNavigator() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="chats" component={Chats} />
-      <Tab.Screen name="calls" component={Calls} />
-      <Tab.Screen name="contactss" component={Contacts} />
+    <Tab.Navigator
+      initialRouteName="Chats"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+      // screenOptions={({route}) => ({
+      //   tabBarIcon: ({focused, color, size}) => {
+      //     let iconName;
+
+      //     if (route.name === 'Chats') {
+      //       iconName = focused ? 'chat' : 'chat-outline';
+      //     } else if (route.name === 'Calls') {
+      //       iconName = focused ? 'phone' : 'phone-outline';
+      //     } else if (route.name === 'Contacts') {
+      //       iconName = focused ? 'contacts' : 'contacts-outline';
+      //     }
+
+      //     // You can return any component that you like here!
+      //     return (
+      //       <MaterialCommunityIcons name={iconName} size={size} color={color} />
+      //     );
+      //   },
+      // })}
+      // tabBarOptions={{
+      //   activeTintColor: 'dodgerblue',
+      //   inactiveTintColor: 'gray',
+      // }}
+    >
+      <Tab.Screen
+        name="chats"
+        component={Chats}
+        options={{
+          tabBarLabel: 'Chat',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="chat" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="calls"
+        component={Calls}
+        options={{
+          tabBarLabel: 'Calls',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="phone" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="contacts"
+        component={Contacts}
+        options={{
+          tabBarLabel: 'Contacts',
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="contacts" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- screens/Chats.js
+++ screens/Chats.js
@@ -1,10 +1,115 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import * as Icons from '../components/icons';
+import {Fonts, FontSize} from '../components/theme';
 
 export default function Chats({navigation}) {
   return (
-    <View>
-      <Text>Chats</Text>
+    <View style={styles.container}>
+      <View style={styles.topHeader}>
+        <View style={styles.leftIcons}>
+          <Icons.SimpleLineIcons name="bell" size={30} color="black" />
+        </View>
+        <View style={styles.logoImage}>
+          <Image
+            source={require('../assets/new.png')}
+            style={{resizeMode: 'contain', height: 50, width: 50}}
+          />
+          <Text
+            style={{
+              position: 'absolute',
+              left: 35,
+              top: 35,
+              backgroundColor: '#51D764',
+              borderRadius: 50,
+              height: 12,
+              width: 12,
+            }}></Text>
+        </View>
+        <View style={styles.rightIcons}>
+          <Icons.MaterialCommunityIcons
+            name="video-outline"
+            size={35}
+            color="black"
+            style={styles.rightIconsStyle}
+          />
+          <Icons.EvilIcons
+            name="search"
+            size={35}
+            color="black"
+            style={styles.rightIconsStyle}
+          />
+          <Icons.MaterialCommunityIcons
+            name="dots-vertical"
+            size={30}
+            color="black"
+            style={styles.rightIconsStyle}
+          />
+        </View>
+      </View>
+      <View style={styles.chatViewContainer}>
+        <View style={styles.chatImage}>
+          <Image
+            source={require('../assets/new.png')}
+            style={{resizeMode: 'contain', height: 50, width: 50}}
+          />
+        </View>
+        <View style={styles.chatStyle}>
+          <Text style={styles.nameStyle}>Talha Mansoor</Text>
+          <Text style={styles.msgStyle}>Talha Mansoor</Text>
+        </View>
+        <View style={styles.dateStyle}>
+          <Text>11/30/2020</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  topHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '10%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#BDBDBD',
+  },
+  leftIcons: {marginLeft: '3%'},
+  logoImage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: '25%',
+  },
+  rightIcons: {
+    flexDirection: 'row',
+    marginRight: '3%',
+    alignItems: 'center',
+  },
+  rightIconsStyle: {
+    paddingHorizontal: '1%',
+  },
+  chatViewContainer: {
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    marginVertical: '2%',
+  },
+  chatStyle: {
+    justifyContent: 'center',
+    marginRight: '15%',
+  },
+  dateStyle: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: '2%',
+  },
+  nameStyle: {fontSize: FontSize.font27, fontWeight: 'bold'},
+  msgStyle: {fontSize: FontSize.font22},
+  chatImage: {},
+});
